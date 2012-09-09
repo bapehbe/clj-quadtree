a25ce860aba47db1d254a0632f8af8f9881712be
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-09-09 11:05:58 +0400

    cache nodes using memoization of create-node almost 3 times faster on multiple calls (13ms vs 36ms)

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject clj-quadtree "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "spatial search using quadtree"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cljts "0.1.0"]
                  [midje "1.4.0"]
-                 [criterium "0.3.0"]]
+                 [criterium "0.3.0"]
+                 [org.clojure/core.memoize "0.5.2"]]
   ;; :jvm-opts ["-Xmx16g"]
   )
