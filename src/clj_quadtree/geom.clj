b679b00400cd29d40fc9cf8aa58ba4ec6f504908
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-09-13 00:32:58 +0400

    added affine transformations, will need them to transform inputs and outputs to search

--- src/clj_quadtree/geom.clj
+++ src/clj_quadtree/geom.clj
@@ -1,5 +1,8 @@
 (ns clj-quadtree.geom
-  (:use [cljts.geom :only [c linear-ring polygon]]))
+  (:use [cljts.geom :only [c linear-ring polygon]])
+  (:import [com.vividsolutions.jts.geom.util
+            AffineTransformation AffineTransformationBuilder]
+           [com.vividsolutions.jts.geom Coordinate Geometry]))
 
 (defn quad->shape [{:keys [x y side]}]
   (polygon (linear-ring (let [x1 (+ x side)
@@ -9,3 +12,36 @@
                               se (c x1 y1)
                               sw (c x y1)]
                           [nw ne se sw nw])) nil))
+
+(defn- create-builder [c1 c2 c3 c4 c5 c6]
+  (com.vividsolutions.jts.geom.util.AffineTransformationBuilder. c1 c2 c3 c4 c5 c6))
+
+(defn- create-transformation [coordinates]
+  (let [builder (apply create-builder (map #(apply c %) (partition 2 coordinates)))]
+    (.getTransformation builder)))
+
+(defprotocol Transformable
+  (transform [this transformation]))
+
+(extend-type Coordinate
+  Transformable
+  (transform [this transformation]
+    (.transform transformation this (Coordinate.))))
+
+(extend-type Geometry
+  Transformable
+  (transform [this transformation]
+    (.transform transformation this)))
+
+(defn- transformation-fn [tr]
+  (fn [this]
+      (transform this tr)))
+
+(defn transformation [coordinates]
+  (transformation-fn (create-transformation coordinates)))
+
+(defn inverse-transformation [coordinates]
+  (transformation-fn (.getInverse (create-transformation coordinates))))
+
+(defn identity-transformation []
+  (transformation-fn (.setToIdentity (AffineTransformation.))))
--- test/clj_quadtree/geom_test.clj
+++ test/clj_quadtree/geom_test.clj
@@ -1,7 +1,7 @@
 (ns clj-quadtree.geom-test
   (:use clj-quadtree.geom
         midje.sweet
-        [cljts.geom :only [c linear-ring polygon]]))
+        [cljts.geom :only [c point linear-ring polygon]]))
 
 (fact
  (quad->shape {:x 0 :y 0 :side 10}) =>
@@ -10,3 +10,58 @@
                                  (c 10 10)
                                  (c 0 10)
                                  (c 0 0)]) nil))
+
+(fact
+ (let [rotation 
+       (transformation '(0 0    1 0    0 1
+                         0 0    0 1   -1 0))]
+   (rotation (c 0 0)) => (c 0 0)
+   (rotation (c 1 0)) => (c 0 1)
+   (rotation (c 0 1)) => (c -1 0)))
+
+(fact
+ (let [rotation 
+       (transformation '(0 0    1 0    0 1
+                         0 0    1 1   -1 1))]
+   (rotation (point (c 0 0))) => (point (c 0 0))
+   (rotation (point (c 1 0))) => (point (c 1 1))
+   (rotation (point (c 0 1))) => (point (c -1 1))))   
+
+(fact
+ (let [scale 
+       (transformation '(0 0    1 0    0 1
+                         0 0    2 1    0 2))]
+   (scale (c 0 0)) => (c 0 0)
+   (scale (c 1 0)) => (c 2 1)
+   (scale (c 0 1)) => (c 0 2)))
+
+(fact
+ (let [translate 
+       (transformation '(0 0    1 0    0 1
+                         5 6    6 6    5 7))]
+   (translate (c 0 0)) => (c 5 6)
+   (translate (c 1 0)) => (c 6 6)
+   (translate (c 0 1)) => (c 5 7)))
+
+(fact
+ (let [linear 
+       (transformation '(0 0    1 0    0 1
+                         0 0    0 0    5 7))]
+   (linear (c 0 0)) => (c 0 0)
+   (linear (c 1 0)) => (c 0 0)
+   (linear (c 0 1)) => (c 5 7)))
+
+(fact
+ (let [inverse 
+       (inverse-transformation '(0 0    1 0    0 1
+                                 0 0    2 1    0 2))]
+   (inverse (c 0 0)) => (c 0 0)
+   (inverse (c 2 1)) => (c 1 0)
+   (inverse (c 0 2)) => (c 0 1)))
+
+(fact
+ (let [identity (identity-transformation)
+       coord (c (rand) (rand))]
+   (identity coord) => coord))
+
+
