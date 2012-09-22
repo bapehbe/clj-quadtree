ec0bdad63a6776e6b4ad1510f107031c807b6fa3
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-09-22 20:03:12 +0400

    search now returns tiles

--- src/clj_quadtree/core.clj
+++ src/clj_quadtree/core.clj
@@ -55,22 +55,36 @@
         seast (create-node-fn nlvl (+ x ns) (+ y ns) ns)]
     (sort-by :id [nwest neast swest seast])))
 
+(defn- children-tiles [create-node-fn depth parent]
+  (let [[px py] (coords parent)
+        plvl (level parent)
+        pside (side parent)
+        side (bit-shift-right pside (- depth plvl))
+        mx (+ px pside)
+        my (+ py pside)]
+    (for [x (range px mx side)
+          y (range py my side)]
+      (create-node-fn depth x y side))))
+
 (defn- search-quads* [create-node-fn ^long depth root s]
   (let [ps (prep/prepare s) ]
     (loop [quads (create-children create-node-fn root)
-           result []
+           result (transient [])
            lvl 1]
       (let [candidates (r/filter #(rel/intersects? ps (shape %)) quads)]
         (if (= lvl depth)
-          (into result candidates)
+          (persistent! (reduce conj! result candidates))
           (let [{covered true touched false}
                 (group-by #(rel/covers? ps (shape %)) candidates)
                 candidates (r/flatten
                             (r/map
                              (partial create-children create-node-fn) touched))]
             ;; don't need to check the covered anymore but touched can
-            ;; be subdivided
-            (recur candidates (into result covered) (inc lvl))))))))
+            ;; be subdivided. Just need to split the covered into tiles
+            (recur candidates
+                   (reduce conj! result
+                           (r/flatten (r/map (partial children-tiles create-node-fn depth) covered)))
+                   (inc lvl))))))))
 
 (defn- memoize-fn [fn cache-method cache-size]
   (if (nil? cache-size)
@@ -84,19 +98,6 @@
 
 (def search
   (create-search-fn default-config))
-
-(defn- children-tile-ids* [parent depth tile-size]
-  (let [[px py] (coords parent)
-        pside (side parent)
-        max-x (+ px pside)
-        max-y (+ py pside)
-        npx (quot px tile-size)
-        npy (quot py tile-size)
-        nmx (quot max-x tile-size)
-        nmy (quot max-y tile-size)]
-    (for [nx (range npx nmx)
-          ny (range npy nmy)]
-      (xy->hilbert nx ny depth))))
 
 (defn make-ranges [ids]
   (loop [start (first ids)
@@ -117,15 +118,5 @@
                          prev
                          [start prev])) true))))))
 
-(defn- tile-ids [{:keys [depth tile-size]} children-tile-ids-fn r]
-  (-> (map (fn [node]
-                  (if (< (level node) depth)
-                    (children-tile-ids-fn node depth tile-size)
-                    (id node))) r) flatten sort make-ranges))
-
-(defn create-search-ids-fn [{:keys [cache-method cache-size] :as config}]
-  (let [search-fn (create-search-fn config)
-        children-tile-ids-fn (memoize-fn children-tile-ids* cache-method cache-size)]
-    (comp (partial tile-ids config children-tile-ids-fn) search-fn)))
-
-(def find-ids (create-search-ids-fn default-config))
+(defn tile-ids [r]
+  (make-ranges (sort (map id r))))
--- test/clj_quadtree/core_test.clj
+++ test/clj_quadtree/core_test.clj
@@ -23,14 +23,8 @@
        p1 (g/point (g/c 2 2))
        result (search p1)]
    (count result) => 1
-   (coords (first result)) => [0 0]))
-
-(fact "search for a point inside a quad will return this quad"
-      (let [search-ids (create-search-ids-fn {:cache-method clojure.core.memoize/memo
-                                              :depth 1
-                                              :tile-size 4})
-            p1 (g/point (g/c 2 2))]
-        (search-ids p1) => [0]))
+   (coords (first result)) => [0 0]
+   (tile-ids result) => [0]))
 
 (fact "search for a line intersecting 2 quads will return these quads"
  (let [search (create-search-fn {:cache-method clojure.core.memoize/memo
@@ -40,14 +34,8 @@
        result (search l1)]
    (count result) => 2
    (coords (first result)) => [0 0]
-   (coords (second result)) => [4 0]))
-
-(fact "search for a line intersecting 2 quads will return these quads"
-      (let [search-ids (create-search-ids-fn {:cache-method clojure.core.memoize/memo
-                                              :depth 1
-                                              :tile-size 4})
-            l1 (g/line-string [(g/c 2 2) (g/c 6 2)])]
-        (search-ids l1) => [0 3]))
+   (coords (second result)) => [4 0]
+   (tile-ids result) => [0 3]))
 
 (fact "search for a circle intersecting 4 quads will return these quads"
  (let [search (create-search-fn {:cache-method clojure.core.memoize/memo
@@ -55,31 +43,18 @@
                                  :tile-size 4})
        c1 (a/buffer (g/point (g/c 3 3)) 2)
        result (search c1)]
-   (count result) => 4))
-
-(fact "search for a circle intersecting 4 quads will return these quads"
-      (let [search-ids (create-search-ids-fn {:cache-method clojure.core.memoize/memo
-                                              :depth 1
-                                              :tile-size 4})
-            c1 (a/buffer (g/point (g/c 3 3)) 2)]
-        (search-ids c1) => [[0 3]]))
+   (count result) => 4
+   (tile-ids result) => [[0 3]]))
 
 (fact
  (let [p1 (g/point (g/c 443 426))
        c1 (a/buffer p1 341)
        search (create-search-fn {:cache-method clojure.core.memoize/memo
                                  :depth 15
-                                 :tile-size 64})]
-   (sort (map id (search c1))) => '(2 6 9 10 11 12 17 18 22 23 28 28 29 30 30 31 31 32 52 53 53 54 54 55 56 57 61 62 66 67 68 69 70 71 72 73 74 97 110 111 116 117 118 119 132 133 134 135 136 139 140 141 142 143 144 208 209 210 211 220 221 222 223 224 226 227 228 229 230 231 233)
-   (map id (filter #(= 15 (level %)) (search c1))) => '(9 10 11 17 18 22 23 28 29 30 31 52 53 54 55 56 57 61 62 66 67 68 69 70 71 72 73 74 97 110 111 116 117 118 119 132 133 134 135 136 139 140 141 142 143 144 208 209 210 211 220 221 222 223 224 226 227 228 229 230 231 233)))
-
-(fact
- (let [p1 (g/point (g/c 443 426))
-       c1 (a/buffer p1 341)
-       search-ids (create-search-ids-fn {:cache-method clojure.core.memoize/memo
-                                 :depth 15
-                                 :tile-size 64})]
-   (search-ids c1) => [[9 11] [17 18] [22 57] [61 62] [66 74] 97 [110 136] [139 144] [208 224] [226 231] 233]))
+                                 :tile-size 64})
+       result (search c1)]
+   (sort (map id result)) => '(9 10 11 17 18 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 61 62 66 67 68 69 70 71 72 73 74 97 110 111 112 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 128 129 130 131 132 133 134 135 136 139 140 141 142 143 144 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223 224 226 227 228 229 230 231 233)
+   (tile-ids result) => [[9 11] [17 18] [22 57] [61 62] [66 74] 97 [110 136] [139 144] [208 224] [226 231] 233]))
 
 (defn- random-ids [n]
   (reduce
