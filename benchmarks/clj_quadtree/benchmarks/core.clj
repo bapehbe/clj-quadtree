8eb59f55f8fae02ecf9d900984f19c0d6273d7ca
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-09-22 20:00:08 +0400

    more 'real-life' benchmark

--- benchmarks/clj_quadtree/benchmarks/core.clj
+++ benchmarks/clj_quadtree/benchmarks/core.clj
@@ -5,18 +5,16 @@
            [cljts.geom :as g]))
 
 (defgoal search-goal "search")
-(def p1 (g/point (g/c 43 46)))
-(def c1 (a/buffer p1 18))
+(def p1 (g/point (g/c 443 426)))
+(def c1 (a/buffer p1 15))
 (def config {:cache-method clojure.core.memoize/memo
-             :depth 12
-             :tile-size 1})
+             :depth 15
+             :tile-size 64})
 (def no-cache-config {:cache-method identity
-                      :depth 12
-                      :tile-size 1})
+                      :depth 15
+                      :tile-size 64})
 (def cache-search (create-search-fn config))
 (def no-cache-search (create-search-fn no-cache-config))
-(def search-ids (create-search-ids-fn config))
-(def no-cache-search-ids (create-search-ids-fn no-cache-config))
 
 (defcase search-goal :point []
   (cache-search p1))
@@ -28,7 +26,4 @@
   (no-cache-search c1))
 
 (defcase search-goal :tile-ids []
-  (search-ids c1))
-
-(defcase search-goal :tile-ids-no-cache []
-  (no-cache-search-ids c1))
+  (tile-ids (cache-search c1)))
