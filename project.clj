632755c0126ee74794d5d68e8046d53f16922c85
Alexey Pushkin <alexey.pushkin@gmail.com>
2013-04-29 23:08:46 +0400

    clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [cljts "0.2.0"]
                  [midje "1.4.0"]
                  [perforate "0.2.3"]
